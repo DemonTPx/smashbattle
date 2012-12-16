eba0889206839911ec17dcd52190454d406d39a7
Ray Burgemeestre <ray.burgemeestre@gmail.com>
2012-12-16 18:13:16 +0100

    Implemented Client object. Used for each connection by the Server. All information related to each client/connection is stored in there. It also processes commands received from the clients.

--- Battle/Client.cpp
+++ Battle/Client.cpp
@@ -1,29 +1,165 @@
 #include "Client.h"
+#include "Server.h"
+#include "Commands.hpp"
+#include "log.h"
 
-Client::Client(int client_id, TCPsocket socket)
-	: client_id_(client_id),
-	  socket_(socket),
-	  buffer_idx_(0),
-	  test(0)
+
+
+/**
+ * This constructor is required for usage in a std::map by the server
+ * Only accessable by it through a friend relation
+ */
+Client::Client() 
+	: CommandProcessor(NULL),
+	  test(0),
+
+	  lag_(INITIAL_LAG_TESTS), 
+	  lastLagTime_(0),
+	  initialLagTests_(INITIAL_LAG_TESTS),
+	  server_(NULL),
+
+	  currentState_(Client::State::CONNECTING)
+	  
+{}	
+
+Client & Client::operator=(Client&& other)
 {
-	memset(buffer_, 0x00, sizeof(buffer_));
+	// The parameters from 'normal' constructor
+	this->client_id_ = other.client_id_;
+	this->set_socket(other.socket_);
+	this->server_ = other.server_;
+	
+	return *this; 
+}
+
+/**
+ * Our intended constructor :P
+ */
+Client::Client(int client_id, TCPsocket socket, Server * const server)
+	: CommandProcessor(socket),
+	  
+	  client_id_(client_id),
+	  character_(0),
+	  server_(server) ,
+	  test(0),
+
+	  lag_(INITIAL_LAG_TESTS),
+	  lastLagTime_(0),
+	  initialLagTests_(10),
+
+	  currentState_(Client::State::CONNECTING)
+	  
+{
+}
+
+bool Client::process(std::unique_ptr<Command> command)
+{
+	Command *cmd = command.get();
+
+	switch (cmd->getType())
+	{
+		case Command::Types::Ping:
+			return process(dynamic_cast<CommandPing *>(cmd));
+		case Command::Types::Pong:
+			return process(dynamic_cast<CommandPong *>(cmd));
+		case Command::Types::SetCharacter:
+			return process(dynamic_cast<CommandSetCharacter *>(cmd));
+		case Command::Types::SetPlayerData:
+			return process(dynamic_cast<CommandSetPlayerData *>(cmd));
+		//case Command::Types::AddPlayer:
+		//	return process(dynamic_cast<CommandAddPlayer *>(cmd));
+	}
+
+	return true;
+}
+
+bool Client::process(CommandPing *command)
+{
+	CommandPong response;
+	response.data.time = command->data.time;
+
+	send(response);
+
+	return true;
+}
+
+bool Client::process(CommandPong *command)
+{
+	float lag = static_cast<float>((server_->getServerTime() - command->data.time) / 2.);
+
+	lag_.add(lag);
+
+	return true;
+}
+
+bool Client::process(CommandSetCharacter *command)
+{
+	char character = command->data.character;
+	character_ = character;
+
+	setState(Client::State::CHARACTER_INITIALIZED);
+
+	return true;
 }
 
 
-void Client::receive(int bytes_received, const char * const buffer)
+// Todo: refactor this
+#include "ServerClient.h" // flags should not be inside here
+#include "NetworkMultiplayer.h"
+bool Client::process(CommandSetPlayerData *command)
 {
-	memcpy(buffer_ + buffer_idx_,
-					buffer,
-					bytes_received);
-					
-	buffer_idx_ += bytes_received;
-	
-	printf("Client received: %s\n", buffer);
-	short *test2 = (short *)buffer_;
-	printf("short recvd: %d\n", *test2);
+	Player *updatedPlayer = NULL;
+	auto playersvec = *(server_->getGame().players);
+	for (auto i = playersvec.begin(); i != playersvec.end(); i++)
+	{
+		auto &player = **i;
+		if (player.number == (int)command->data.client_id)
+		{
+			player_util::set_player_data(player, *command);
 
-	test = *test2;
+			updatedPlayer = &player;
+		}
+	}
 
-	buffer_idx_ = 0;// temp hack
-	// Todo: Process here!! Now buffer_ will overflow  :)
+	// Notify other players of this new update
+	if (!updatedPlayer)
+		return true;
+
+	for (auto i = playersvec.begin(); i != playersvec.end(); i++)
+	{
+		auto &player = **i;
+		if (player.number != (int)command->data.client_id)
+		{
+			CommandSetPlayerData data;
+
+			player_util::set_position_data(data, updatedPlayer->number, server_->getServerTime(), *updatedPlayer);
+
+			server_->getClientById(player.number).send(data);
+		}
+	}
+	return true;
+}
+
+
+void Client::send(Command &command)
+{
+	//	if (!is_connected_)
+	//    return;
+
+	char type = command.getType();
+	int result = SDLNet_TCP_Send(socket_, &type, sizeof(char));
+
+	if(result < sizeof(char)) {
+		if(SDLNet_GetError() && strlen(SDLNet_GetError())) /* sometimes blank! */
+			printf("SDLNet_TCP_Send: %s\n", SDLNet_GetError());
+		return;
+	}
+
+	result = SDLNet_TCP_Send(socket_, command.getData(), command.getDataLen());
+
+	if(result < sizeof(socket_)) {
+		if(SDLNet_GetError() && strlen(SDLNet_GetError())) /* sometimes blank! */
+			printf("SDLNet_TCP_Send: %s\n", SDLNet_GetError());
+		return;
+	}
 }
--- Battle/Client.h
+++ Battle/Client.h
@@ -4,31 +4,75 @@
 #include "SDL/SDL_net.h"
 #include <string>
 #include <map>
+#include <memory>
 
-class Client
+#include "CommandProcessor.h"
+
+#include "util/LagMeasure.hpp"
+
+class CommandPing;
+class CommandPong;
+class CommandSetCharacter;
+class CommandSetPlayerData;
+class CommandSetClientData;
+class Server;
+
+class Client : public CommandProcessor
 {
 public:
 
-	Client(int client_id, TCPsocket socket);
-	
-	void receive(int bytes_received, const char * const buffer);
+	enum State
+	{
+		CONNECTING = 1,
+		CHARACTER_REQUESTED = 2,
+		CHARACTER_INITIALIZED = 3,
+		ACTIVE = 4,
+	};
 
-	
+
+	Client(int client_id, TCPsocket socket, Server * const server);
+	Client & operator=(Client&& other);
+
+	bool process(std::unique_ptr<Command> command);
+	bool process(CommandPing *command);
+	bool process(CommandPong *command);
+	bool process(CommandSetCharacter *command);
+	bool process(CommandSetPlayerData *command);
+
 	// accessors
 	TCPsocket socket() { return socket_; }
 	int id() { return client_id_; }
+	int getClientId() { return client_id_; }
+	Client::State getState() { return currentState_; }
+	void setState(Client::State state) { currentState_ = state; }
+	char getCharacter() { return character_; }
+
+	// communication
+	void send(Command &command);
 	
+	// lag
+	Uint32 getLastLagTime() { return lastLagTime_; }
+	void setLastLagTime(Uint32 time) { lastLagTime_ = time; }
+	int getInitialLagTests() { return initialLagTests_; }
+	void setInitialLagTests(int v) { initialLagTests_ = v; }
+
 	short test;
 
 private:
 	
 	friend std::map<int, Client>;
-	Client() {}	
+	Client();
 	
 	int client_id_;
-	TCPsocket socket_;
-	char buffer_[1024];
-	int buffer_idx_;
+	char character_;
+
+	Server * server_;
+
+	LagMeasure lag_;
+	Uint32 lastLagTime_;
+	int initialLagTests_;
+
+	Client::State currentState_;
 };
 
 #endif //__CLIENT_H__
