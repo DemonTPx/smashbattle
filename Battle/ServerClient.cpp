6db32ff39b54372536b5f00dba4c57b6569af89f
Ray Burgemeestre <ray.burgemeestre@gmail.com>
2012-12-16 18:16:28 +0100

    Further implemented client

--- Battle/ServerClient.cpp
+++ Battle/ServerClient.cpp
@@ -5,8 +5,21 @@
 
 #include "ServerClient.h"
 
+#include "ClientNetworkMultiplayer.h"
+#include "Level.h"
+
+#include "log.h"
+
 ServerClient::ServerClient()
-	: is_connected_(false)
+	: is_connected_(false),
+	  currentState_(ServerClient::State::INITIALIZING),
+	  host_("localhost"),
+	  port_((Uint16)1099),
+	  CommandProcessor(0),
+	  lag(INITIAL_LAG_TESTS),
+	  game_(NULL),
+	  level_(NULL),
+	  my_id_(0x00)
 {
 }
 
@@ -15,40 +28,43 @@ ServerClient::~ServerClient()
 	SDLNet_Quit();
 }
 
-void ServerClient::connect()
+void ServerClient::connect(ClientNetworkMultiplayer &game, Level &level, Player &player)
 {
 	if (is_connected_)
 		return;
 
+	game_ = &game;
+	level_ = &level;
+	player_ = &player;
+
+	log(format("CONNECTING TO %s:%d...",host_.c_str(),port_), Logger::Priority::CONSOLE);
+
 	/* initialize SDL */
 	if(SDL_Init(0)==-1)
 	{
-		printf("SDL_Init: %s\n",SDL_GetError());
+		log(format("SDL_Init: %s\n",SDL_GetError()), Logger::Priority::ERROR);
 		return;
 	}
 
 	/* initialize SDL_net */
 	if(SDLNet_Init()==-1)
 	{
-		printf("SDLNet_Init: %s\n",SDLNet_GetError());
+		log(format("SDLNet_Init: %s\n",SDLNet_GetError()), Logger::Priority::ERROR);
 		return;
 	}
 
 	set=SDLNet_AllocSocketSet(1);
 	if(!set)
 	{
-		printf("SDLNet_AllocSocketSet: %s\n", SDLNet_GetError());
+		log(format("SDLNet_AllocSocketSet: %s\n", SDLNet_GetError()), Logger::Priority::ERROR);
 		return;
 	}
 
-	/* get the port from the commandline */
-	port=(Uint16)1099;
-	
+
 	/* Resolve the argument into an IPaddress type */
-	printf("Connecting to %s port %d\n","localhost",port);
-	if(SDLNet_ResolveHost(&ip,"localhost",port)==-1)
+	if(SDLNet_ResolveHost(&ip,host_.c_str(),port_)==-1)
 	{
-		printf("SDLNet_ResolveHost: %s\n",SDLNet_GetError());
+		log(format("SDLNet_ResolveHost: %s\n",SDLNet_GetError()), Logger::Priority::ERROR);
 		return;
 	}
 
@@ -56,17 +72,22 @@ void ServerClient::connect()
 	sock=SDLNet_TCP_Open(&ip);
 	if(!sock)
 	{
-		printf("SDLNet_TCP_Open: %s\n",SDLNet_GetError());
+		log(format("SDLNet_TCP_Open: %s\n",SDLNet_GetError()), Logger::Priority::ERROR);
 		return;
 	}
 	
 	if(SDLNet_TCP_AddSocket(set,sock)==-1)
 	{
-		printf("SDLNet_TCP_AddSocket: %s\n",SDLNet_GetError());
+		log(format("SDLNet_TCP_AddSocket: %s\n",SDLNet_GetError()), Logger::Priority::ERROR);
 		return;
 	}
 	
-	printf("Ready\n");
+	
+
+	log(format("CONNECTION SUCCESFUL",host_.c_str(),port_), Logger::Priority::CONSOLE);
+
+	set_socket(sock);
+
 	is_connected_ = true;
 }
 
@@ -94,8 +115,9 @@ void ServerClient::poll(short test)
 
 		if (bytesReceived > 0)
 		{
-			printf("@todo impl@received bytes from server.\n");
-			//bytesReceived, buffer
+			receive(bytesReceived, buffer);
+			while (parse())
+					;
 		}
 		else
 		{
@@ -109,12 +131,170 @@ void ServerClient::poll(short test)
 
 	}
 	
-	// test string to server (once)
 
-	result=SDLNet_TCP_Send(sock, (const void *)&test,sizeof(short));
-	if(result<sizeof(short)) {
+}
+
+#include "Commands.hpp"
+#include "log.h"
+void ServerClient::send(Command &command)
+{
+	if (!is_connected_)
+		return;
+
+	char type = command.getType();
+	result = SDLNet_TCP_Send(sock, &type, sizeof(char));
+
+	if(result < sizeof(char)) {
 		if(SDLNet_GetError() && strlen(SDLNet_GetError())) /* sometimes blank! */
 			printf("SDLNet_TCP_Send: %s\n", SDLNet_GetError());
 		return;
 	}
+
+	result = SDLNet_TCP_Send(sock, command.getData(), command.getDataLen());
+
+	if(result < sizeof(char)) {
+		if(SDLNet_GetError() && strlen(SDLNet_GetError())) /* sometimes blank! */
+			printf("SDLNet_TCP_Send: %s\n", SDLNet_GetError());
+		return;
+	}
+}
+void ServerClient::test()
+{
+	CommandPing command;
+	command.data.time = SDL_GetTicks();
+
+	this->send(command);
+}
+
+
+bool ServerClient::process(std::unique_ptr<Command> command)
+{
+	
+
+	switch (command.get()->getType())
+	{
+		case Command::Types::Ping:
+			process(dynamic_cast<CommandPing *>(command.get()));
+			break;
+		case Command::Types::Pong:
+			process(dynamic_cast<CommandPong *>(command.get()));
+			break;
+		case Command::Types::SetLevel:
+			process(dynamic_cast<CommandSetLevel *>(command.get()));
+			break;
+		case Command::Types::RequestCharacter:
+			process(dynamic_cast<CommandRequestCharacter *>(command.get()));
+			break;
+		//case Command::Types::SetCharacter:
+			//process(dynamic_cast<CommandSetCharacter *>(command.get()));
+			//break;
+		case Command::Types::SetPlayerData:
+			process(dynamic_cast<CommandSetPlayerData *>(command.get()));
+			break;
+		case Command::Types::AddPlayer:
+			process(dynamic_cast<CommandAddPlayer *>(command.get()));
+			break;
+		default:
+			log(format("received command with type: %d", command.get()->getType()), Logger::Priority::CONSOLE);
+	}
+
+	return true;
+}
+
+bool ServerClient::process(CommandPing *command)
+{
+	CommandPong reply;
+	reply.data.time = SDL_GetTicks();
+
+	this->send(reply);
+
+	log(format("PING? PONG!"), Logger::Priority::CONSOLE);
+
+	return true;
+}
+
+bool ServerClient::process(CommandPong *command)
+{
+	float measuredLag = static_cast<float>((SDL_GetTicks() - command->data.time) / 2.0);
+	
+	lag.add(measuredLag);
+
+	log(format("LAG %f AVG=%f", measuredLag, lag.avg()), Logger::Priority::CONSOLE);
+	return true;
+}
+
+
+bool ServerClient::process(CommandSetLevel *command)
+{
+	my_id_ = command->data.your_id;
+
+	// Default a client gets player with number zero
+	player_->number = my_id_;
+
+	level_->load(level_util::get_filename_by_name(command->data.level).c_str());
+			
+	game_->set_level(level_);
+	
+	log(format("my id is: %d load level %s!", command->data.your_id, command->data.level), Logger::Priority::CONSOLE);
+
+	setState(ServerClient::State::INITIALIZED);
+
+	return true;
+}
+
+bool ServerClient::process(CommandRequestCharacter *command)
+{
+	log(format("SERVER REQUESTED CHARACTER"), Logger::Priority::CONSOLE);
+
+	CommandSetCharacter response;
+	response.data.time = command->data.time;
+	// response.data.nickname
+	response.data.character = player_->character;
+
+	this->send(response);
+
+	return true;
+}
+
+
+bool ServerClient::process(CommandSetPlayerData *command)
+{
+	log(format("Server send player data"), Logger::Priority::DEBUG);
+
+	if (my_id_ == command->data.client_id)
+	{
+		player_->position->x = command->data.x;
+		player_->position->y = command->data.y;
+		//player_util::set_player_data(player_, *command);
+	}
+	else
+	{
+		auto &players = *(game_->players);
+		for (auto i=players.begin(); i!=players.end(); i++)
+		{
+			auto &otherplayer = **i;
+
+			if (otherplayer.number == command->data.client_id)
+			{
+				player_util::set_player_data(otherplayer, *command);
+			}
+		}
+	}
+
+	return true;
+}
+
+bool ServerClient::process(CommandAddPlayer *command)
+{
+	Player *otherplayer = new Player(command->data.character, command->data.client_id);
+	GameInputStub *playerinput = new GameInputStub();
+	otherplayer->input = playerinput;
+
+	otherplayer->position->x = command->data.x;
+	otherplayer->position->y = command->data.y;
+	otherplayer->set_sprite((command->data.facingRight ? SPR_R : SPR_L));
+	otherplayer->reset();
+			
+	game_->add_player(otherplayer);
+	return true;
 }
--- Battle/ServerClient.h
+++ Battle/ServerClient.h
@@ -1,6 +1,8 @@
 #ifndef __SERVER_CLIENT_H__
 #define __SERVER_CLIENT_H__
 
+#include <string>
+
 #include <string.h>
 #include <sys/types.h>
 #ifndef WIN32
@@ -15,7 +17,25 @@
 #include "SDL/SDL_thread.h"
 #include "SDL/SDL_net.h"
 
-class ServerClient
+
+#include "CommandProcessor.h"
+#include "util/LagMeasure.hpp"
+
+
+class ClientNetworkMultiplayer;
+class Level;
+class Player;
+
+class Command;
+class CommandPing ;
+class CommandPong;
+class CommandSetLevel;
+class CommandRequestCharacter;
+class CommandSetCharacter;
+class CommandSetPlayerData;
+class CommandAddPlayer;
+
+class ServerClient : public CommandProcessor
 {
 public:
 	
@@ -30,14 +50,47 @@ public:
 	static const short FLAG_START = 0x200;
 	
 
+	enum State
+	{
+		INITIALIZING = 1,
+		INITIALIZED = 2
+	};
+
+
 	static ServerClient& getInstance()
 	{
 		static ServerClient instance;
 		return instance;
 	}
 
-	void connect();
+	void setHost(std::string host) { host_ = host; }
+	void setPort(int port) { port_ = (Uint16)port; }
+
+	void connect(ClientNetworkMultiplayer &game, Level &level, Player &player);
 	void poll(short test);
+
+	void test();
+	void send(Command &command);
+
+	bool isConnected() { return is_connected_; }
+
+	void setState(ServerClient::State state) { currentState_ = state; }
+	ServerClient::State getState() { return currentState_; }
+
+	LagMeasure &getLag() { return lag; }
+
+	char getClientId() { return my_id_; }
+
+protected:
+	bool process(std::unique_ptr<Command> command);
+	
+	bool process(CommandPing *command);
+	bool process(CommandPong *command);
+	bool process(CommandSetLevel *command);
+	bool process(CommandRequestCharacter *command);
+	bool process(CommandSetPlayerData *command);
+	bool process(CommandAddPlayer *command);
+	// not for server client bool process(CommandSetCharacter *command);
 
 private:
 	ServerClient();
@@ -48,14 +101,34 @@ private:
 
 	bool is_connected_;
 
+	
+	ServerClient::State currentState_;
+
 	IPaddress ip;
 	TCPsocket sock;
-	Uint16 port;
+	std::string host_;
+	Uint16 port_;
 	SDLNet_SocketSet set;
 	fd_set fdset;
 	int result;
 	char *name,*str;
 	struct timeval tv;
+
+	// received
+	char buffer_[8192];
+	int buffer_idx_;
+
+	// send buffer
+	char send_buffer_[8192];
+	int send_buffer_idx_;
+
+	LagMeasure lag;
+
+	char my_id_;
+
+	ClientNetworkMultiplayer * game_;
+	Level * level_;
+	Player * player_;
 };
 
 
