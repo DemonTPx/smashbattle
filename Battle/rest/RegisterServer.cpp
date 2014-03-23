7c731c1ddc99ac7bc5469a966e9cd4d771ea436a
rayburgemeestre <ray.burgemeestre@gmail.com>
2014-03-23 22:31:07 +0100

    Periodically update server data to server asynchronously

--- Battle/rest/RegisterServer.cpp
+++ Battle/rest/RegisterServer.cpp
@@ -30,4 +30,23 @@ std::string RegisterServer::put(Main &main)
 	return tokenString;
 }
 
+void RegisterServer::update(Main &main)
+{
+	future_ = std::async(std::launch::async, [&]{ 
+		json::Object postData;
+		postData["servername"] = main.getServer().getName();
+		// Host will be determined by the API server for now
+		//  postData["host"] = Server::getInstance().getHost();
+		postData["port"] = main.getServer().getPort();
+		postData["level"] = main.getServer().getLevelName();
+		postData["required_players"] = 2;
+		postData["num_active_players"] = (int)main.getServer().numActiveClients();
+		postData["num_joined_players"] = (int)main.getServer().numJoinedClients();
+		
+		std::cout << "post data generated is; " << json::Serialize(postData) << std::endl;
+		json::Object obj = request("PUT", "http://battle.cppse.nl/server/update", serverToken_, json::Serialize(postData));
+		std::string tokenString(obj["random_string"]);
+	});
+}
+
 }
