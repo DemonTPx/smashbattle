0affa3d5cd8613dcbb14a73d04432fe2ef34bd29
rayburgemeestre <ray.burgemeestre@gmail.com>
2014-03-23 15:41:10 +0100

    Forgot to commit the Drawable interfaces

---
+++ Battle/Drawable.cpp
@@ -0,0 +1,14 @@
+#include "Drawable.h"
+#include "Main.h"
+
+#include "SDL/SDL.h"
+
+Drawable::Drawable(Main &main) : main_(main) {}
+
+void Drawable::draw(SDL_Surface * screen, int frames_processed)
+{
+	if (main_.no_sdl)
+		return;
+
+	draw_impl(screen, frames_processed);
+}
